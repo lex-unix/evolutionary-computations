21d185dcd992d7e6e1b2414fea129afd44ba19b1
Lex Miin <alexmiyin@gmail.com>
2023-11-18 12:15:05 +0200

    feat: create config parsed from cli arguments

---
+++ src/config.py
@@ -0,0 +1,31 @@
+import argparse
+from dataclasses import dataclass
+from typing import Union
+
+
+@dataclass
+class Config:
+    algo: str
+    parameter: str
+    plot: bool
+    save_plot: bool
+    output: Union[str, None]
+
+
+def get_config():
+    parser = argparse.ArgumentParser(description="This is an optimization analyzer using the Deformed Stars Method")  # nopep8
+
+    parser.add_argument('-a', '--algo', help='Alogrithm', required=True)
+    # parser.add_argument('-d', '--dimension', type=int, help='Dimensions', required=True)  # nopep8
+    parser.add_argument('-p', '--parameter',
+                        help='Parameter to test', required=True)
+    parser.add_argument(
+        '--plot', type=bool, help='Plot results of optimization', default=True)
+    parser.add_argument(
+        '--output', help='Output result into file. Available formats are: excel and csv', default=False)
+    parser.add_argument('--save-plot', action='store_true',
+                        help='Save output plots')
+
+    args = parser.parse_args()
+
+    return Config(**vars(args))
