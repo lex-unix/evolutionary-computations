86db61586c49fceaf5fcdde91862996cb3f703a2
Lex Miin <alexmiyin@gmail.com>
2023-11-24 17:51:22 +0200

    feat(config): add dimension option, use plot with default value false

--- src/config.py
+++ src/config.py
@@ -10,17 +10,18 @@ class Config:
     plot: bool
     save_plot: bool
     output: Union[str, None]
+    dimension: int
 
 
 def get_config():
     parser = argparse.ArgumentParser(description="This is an optimization analyzer using the Deformed Stars Method")  # nopep8
 
     parser.add_argument('-a', '--algo', help='Alogrithm', required=True)
-    # parser.add_argument('-d', '--dimension', type=int, help='Dimensions', required=True)  # nopep8
+    parser.add_argument('-d', '--dimension', type=int, help='Dimensions', default=2)  # nopep8
     parser.add_argument('-p', '--parameter',
                         help='Parameter to test', required=True)
-    parser.add_argument(
-        '--plot', type=bool, help='Plot results of optimization', default=True)
+    parser.add_argument('--plot', action='store_true',
+                        help='Plot results of optimization')
     parser.add_argument(
         '--output', help='Output result into file. Available formats are: excel and csv', default=False)
     parser.add_argument('--save-plot', action='store_true',
