ed2fb0d6379efff8f4376a15027429f825931f15
Lex Miin <alexmiyin@gmail.com>
2023-11-24 17:53:13 +0200

    feat(constants): add list of 1D and ND test functions

--- src/lib/constants.py
+++ src/lib/constants.py
@@ -1,3 +1,5 @@
-from src.lib.functions import Ackley, Easom, ThreeHumpCamel
+from src.lib.functions import Ackley, Easom, Sphere, Sphere3D, ThreeHumpCamel
 
+FUNCTIONS_1D = [Sphere()]
 FUNCTIONS_2D = [ThreeHumpCamel(), Ackley(), Easom()]
+FUNCTIONS_ND = [Sphere3D()]
