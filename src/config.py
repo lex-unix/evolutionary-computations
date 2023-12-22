78ce603cf04328695d3e28cd063c4a510fd00aec
Lex Miin <alexmiyin@gmail.com>
2023-12-22 17:18:58 +0200

    refactor: rename `get_config` to `read_cli`

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from typing import Union
 
 
 @dataclass
-class Config:
+class CliConfig:
     algo: str
     parameter: str
     plot: bool
@@ -13,20 +13,16 @@ class Config:
     dimension: int
 
 
-def get_config():
-    parser = argparse.ArgumentParser(description="This is an optimization analyzer using the Deformed Stars Method")  # nopep8
+def read_cli() -> CliConfig:
+    parser = argparse.ArgumentParser(description="This is an optimization analyzer using the Deformed Stars Method")
 
     parser.add_argument('-a', '--algo', help='Alogrithm', required=True)
-    parser.add_argument('-d', '--dimension', type=int, help='Dimensions', default=2)  # nopep8
-    parser.add_argument('-p', '--parameter',
-                        help='Parameter to test', required=True)
-    parser.add_argument('--plot', action='store_true',
-                        help='Plot results of optimization')
-    parser.add_argument(
-        '--output', help='Output result into file. Available formats are: excel and csv', default=False)
-    parser.add_argument('--save-plot', action='store_true',
-                        help='Save output plots')
+    parser.add_argument('-d', '--dimension', type=int, help='Dimensions', default=2)
+    parser.add_argument('-p', '--parameter', help='Parameter to test', required=True)
+    parser.add_argument('--plot', action='store_true', help='Plot results of optimization')
+    parser.add_argument('--output', help='Output result into file. Available formats are: excel and csv', default=False)
+    parser.add_argument('--save-plot', action='store_true', help='Save output plots')
 
     args = parser.parse_args()
 
-    return Config(**vars(args))
+    return CliConfig(**vars(args))
